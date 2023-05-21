bf6f568ae890950db3df9b0e210a401073124c17
ChurakovPro <134218454+ChurakovPro@users.noreply.github.com>
2023-05-21 18:18:24 +0300

    Add files via upload

---
+++ Lab4AK.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static int CountFilesInDirectory(string directoryPath)
+    {
+        int fileCount = 0;
+
+        foreach (string file in Directory.EnumerateFiles(directoryPath, "*", SearchOption.AllDirectories))
+        {
+            fileCount++;
+        }
+
+        return fileCount;
+    }
+
+    static void Main(string[] args)
+    {
+        string directoryPath = "C:\\Program Files (x86)\\Microsoft"; 
+
+        int fileCount = CountFilesInDirectory(directoryPath);
+        Console.WriteLine("Количество Файлов: " + fileCount);
+    }
+}
